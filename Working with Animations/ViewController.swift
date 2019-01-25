00cc82df8361502d856beb18762c98ddb5f5f427
Charles Martin Reed <charlesmartinreed@icloud.com>
2019-01-24 19:24:22 -0600

    Initial Commit

---
+++ Working with Animations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Working with Animations
+//
+//  Created by Charles Martin Reed on 1/24/19.
+//  Copyright © 2019 Charles Martin Reed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
